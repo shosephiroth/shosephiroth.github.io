7883b798a559355f720c5ea1f97ae34574a4b7a8
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2020-10-31 14:22:24 -0400

    initialized 3 variables

--- variable.js
+++ variable.js
@@ -1,1 +1,9 @@
-var a = 9; // the sleek way to create a variable & assign value in one line
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
