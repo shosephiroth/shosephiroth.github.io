bcc964c0e16fd3b9dd88ce625239ff01ab00334a
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2020-10-31 14:27:38 -0400

    defined variables; updated variables to camelCase

--- variable.js
+++ variable.js
@@ -1,9 +1,9 @@
-// Only change code below this line
-var a = 5;
-var b = 10;
-var c = "I am a";
-// Only change code above this line
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
 
-a = a + 1;
-b = b + 5;
-c = c + " String!";
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
