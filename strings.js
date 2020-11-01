7c5442f6edc7f8eef33f79996657c5569dde947d
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2020-11-01 06:40:50 -0500

    Escaping literal quotes in strings.
    I did this completely wrong lol. I'm supposed to create a variable that I call while writing the statement out to be printed.

---
+++ strings.js
@@ -0,0 +1,6 @@
+// Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would
+// see:
+
+// I am a "double quoted" string inside "double quotes".
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\""; // Change this line
