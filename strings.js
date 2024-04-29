3f3e5d3f9bf61481f6eb93e5e678f8f11aa4d672
Mattx2k1 <therealmattharris@gmail.com>
2024-04-29 09:39:55 -0400

    removing original files and starting from scratch

--- strings.js
+++
@@ -1,7 +0,0 @@
-// Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would
-// see:
-
-// I am a "double quoted" string inside "double quotes".
-
-var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
-console.log(myStr); // not sure I needed this but I think it works to print IRL
